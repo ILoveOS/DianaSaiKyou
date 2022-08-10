882932b58f736449a8b3b234b969576398237e39
ILoveOS <2929700634@qq.com>
2022-08-10 13:42:09 +0800

    添加了注释

--- src/util/music.js
+++ src/util/music.js
@@ -1,7 +1,14 @@
+/**
+ * 时间类
+ */
 export class Time {
     hour = 0
     min = 0
     sec = 0
+    /**
+     * 根据秒数构造Time对象
+     * @param {number} secs 秒
+     */
     constructor(secs) {
         secs = Math.floor(secs)
         if (secs >= 60) {
@@ -15,6 +22,9 @@ export class Time {
             this.hour = Math.floor(this.min / 60)
         }
     }
+    /**
+     * @returns HH:MM:SS格式
+     */
     toString() {
         let hFormat = ''
         let mFormat = ''
@@ -26,20 +36,31 @@ export class Time {
     }
 }
 
+/**
+ * 页大小
+ */
 export const PAGESIZE = 30
-
+/**
+ * 网易云api
+ */
 const API = {
     djPrograms: 'https://netease-cloud-music-api-orpin-pi.vercel.app/dj/program',
     songUrlPattern: 'https://music.163.com/song/media/outer/url?id=#{id}.mp3'
 }
-
-class music {
+/**
+ * 音乐类
+ */
+class Music {
     id
     name
     description
     duration
     cover
     src
+    /**
+     * 根据api返回的数据构造Music对象
+     * @param {object} o 
+     */
     constructor(o) {
         this.id = o.mainSong.id
         this.name = o.mainSong.name
@@ -49,7 +70,12 @@ class music {
         this.src = API.songUrlPattern.replace('#{id}', o.mainSong.id);
     }
 }
-
+/**
+ * 加载音乐
+ * @param {number} playlistRid 电台节目的rid 
+ * @param {number} curPage 页号
+ * @param {function} callback 回调函数
+ */
 export const loadMusics = (playlistRid, curPage, callback) => {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = () => {
@@ -62,9 +88,9 @@ export const loadMusics = (playlistRid, curPage, callback) => {
                     result.musics = [{}]
                     for (let i = 0; i < res.programs.length; i++) {
                         if (i != 0)
-                            result.musics.push(new music(res.programs[i]))
+                            result.musics.push(new Music(res.programs[i]))
                         else
-                            result.musics[0] = new music(res.programs[i])
+                            result.musics[0] = new Music(res.programs[i])
                     }
                     callback(result)
                 }
@@ -74,7 +100,13 @@ export const loadMusics = (playlistRid, curPage, callback) => {
     xhr.open('GET', `${API.djPrograms}?rid=${playlistRid}&limit=${PAGESIZE}&offset=${curPage * PAGESIZE}`);
     xhr.send();
 }
-
+/**
+ * 将播放进度转化为HH:MM:SS/HH:MM:SS格式的字符串
+ * @param {boolean} canplay 
+ * @param {number} current 
+ * @param {number} duration 
+ * @returns 
+ */
 export const progressTimeFormat = (canplay,current, duration) => {
     if (canplay) {
         let cur = new Time(current), dur = new Time(duration)
--- src/util/service.js
+++ src/util/service.js
@@ -1,4 +1,10 @@
 
+/**
+ * 调用background.js中的service
+ * @param {string} service 服务名
+ * @param {object} params 参数
+ * @param {function} callback 回调函数
+ */
 const callService=(service,params,callback)=>{
     chrome.runtime.sendMessage({
         service:service,
@@ -12,13 +18,29 @@ const callService=(service,params,callback)=>{
     })
 }
 
+/**
+ * 对外的服务包装接口
+ */
 export const Services={
+    /**
+     * 设置选项
+     * @param {object} options 选项 
+     * @param {function} callback 回调函数 
+     */
     setOption(options,callback){
         callService('setOption',{options:options},res=>callback(res))
     },
+    /**
+     * 获取选项
+     * @param {function} callback 回调函数 
+     */
     getOption(callback){
         callService('getOption',{},res=>callback(res))
     },
+    /**
+     * 重置选项
+     * @param {funtion} callback 回调函数 
+     */
     clearOption(callback){
         callService('clearOption',{},res=>callback(res))
     }
