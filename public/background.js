5dc9f0444da4431a9359680605f3da54a88e3eda
ILoveOS <2929700634@qq.com>
2022-08-10 14:00:02 +0800

    添加了注释

--- public/background.js
+++ public/background.js
@@ -1,20 +1,53 @@
+/**
+ * 插件的Service_work
+ * 用于给其他组件提供服务
+ * @author 澄茜早睡早起
+ */
+
+/**
+ * 默认选项
+ */
 const DefaultOption = {
+    //标签页标题
     title: '新标签页',
+    //背景颜色
     backgroundColor: '#FFFFFF',
+    //前景色
     color: '#E799B0',
+    //电台节目rid
     playList: '959370203',
+    //搜索引擎id
     searchTool: 0,
+    //图标
     icon: '/assets/icon.png',
+    //进度条图标
     progressIcon: '/assets/progress.png',
+    //独轮车内容
     wheelText: '嘉然最強！嘉然最強！嘉然最強！嘉然最強！',
+    //独轮车启动图片
     wheelStartImg: chrome.runtime.getURL('/assets/dacall.gif'),
+    //独轮车停止图片
     wheelPauseImg: chrome.runtime.getURL('/assets/dacall.png'),
+    //独轮车频率(ms)
     wheelInterval: 7000
 }
+/**
+ * 服务
+ */
 const Services = {
+    /**
+     * 设置选项
+     * @param {object} params 参数
+     * @param {function} callback 回调函数
+     */
     setOption: (params, callback) => {
         chrome.storage.local.set({ Options: params.options }, callback(success(null, '选项设置成功')))
     },
+    /**
+     * 获取选项
+     * @param {object} params 参数
+     * @param {function} callback 回调函数
+     */
     getOption: (params, callback) => {
         var copy = {}
         for (let i in DefaultOption)
@@ -28,12 +61,32 @@ const Services = {
             callback(success(copy, '选项获取成功'))
         })
     },
+    /**
+     * 重置选项
+     * @param {objcet} params 参数
+     * @param {function} callback 回调函数
+     */
     clearOption: (params, callback) => {
         chrome.storage.local.set({ Options: null }, callback(success(DefaultOption, '选项重置成功')))
     },
 }
+/**
+ * 服务调用成功后返回的数据
+ * @param {object} data 返回数据 
+ * @param {stirng} message 信息 
+ * @returns 
+ */
 const success = (data, message) => { return { code: 0, data: data, message: message } }
+/**
+ * 服务调用失败后返回的数据
+ * @param {number} code 返回代码
+ * @param {string} message 信息
+ * @returns 
+ */
 const error = (code, message) => { return { code: code, data: null, message: message } }
+/**
+ * 与外部通信的监听器
+ */
 chrome.runtime.onMessage.addListener((req, sender, res) => {
     if (req != null) {
         let service = req.service
--- public/inject/wheel.js
+++ public/inject/wheel.js
@@ -1,34 +1,74 @@
+/**
+ * 独轮车的Content-js
+ * 在b站直播间
+ * @author 澄茜早睡早起
+ */
+
+/**
+ * 校验是否是直播间
+ */
 if (location.pathname.match(/\/[0-9]+.*/)) {
     console.log('Loading Wheel')
+    /**
+     * 获取房间号
+     */
     const WHEEL_ROOMID = location.pathname.replace(/\?.*/, '').replace('/', '')
-
+    /**
+     * 弹幕API
+     */
     const WHEEL_API = 'https://api.live.bilibili.com/msg/send'
-
+    /**
+     * 独轮车线程
+     */
     var WHEEL_PROCESS = null
-
+    /**
+     * 创建DOM
+     */
     const WHEEL_WRAPPER = document.createElement('div')
-
     const WHEEL_BUTTON = document.createElement('img')
-
+    /**
+     * 独轮车配置
+     */
     const WHEEL_CONFIG = {
+        //选项是否加载
         WHEEL_LOADED: false,
+        //状态
         WHEEL_RUNNING: false,
+        //频率
         WHEEL_INTERVAL: 7000,
+        //启动图片
         WHEEL_START_IMG: '',
+        //暂停图片
         WHEEL_PAUSE_IMG: ''
     }
 
+    /**
+     * 独轮车数据
+     */
     const WHEEL_DATA = {
         bubble: 5,
+        //内容
         msg: '',
+        //弹幕颜色
         color: 9920249,
+        //弹幕模式
         mode: 4,
+        //字体大小
         fontsize: 25,
+        //随机数
         rnd: null,
+        //房间号
         roomid: WHEEL_ROOMID,
+        //csrf
         csrf: null,
+        //csrf
         csrf_token: null
     }
+
+    /**
+     * 读取csrf
+     * @returns 当前登录用户的csrf或者null 
+     */
 
     const getCSRF = () => {
         let cookies = document.cookie.split(';')
@@ -39,12 +79,19 @@ if (location.pathname.match(/\/[0-9]+.*/)) {
         return null
     }
 
+    /**
+     * 将数据转为FormData
+     * @param {objcet} data 数据 
+     * @returns 
+     */
     const toFormData = data => {
         let formData = new FormData()
         for (let i in data) formData.append(i, data[i])
         return formData
     }
-
+    /**
+     * 通过xhr请求发送弹幕
+     */
     const send = () => {
         WHEEL_DATA.rnd = new Date().getTime().toString().substring(0, 10)
         WHEEL_DATA.csrf = WHEEL_DATA.csrf_token = getCSRF()
@@ -60,6 +107,9 @@ if (location.pathname.match(/\/[0-9]+.*/)) {
         xhr.send(toFormData(WHEEL_DATA))
     }
 
+    /**
+     * 独轮车开关
+     */
     const toggle = () => {
         if (WHEEL_CONFIG.WHEEL_LOADED) {
             if (WHEEL_CONFIG.WHEEL_RUNNING)
@@ -72,11 +122,16 @@ if (location.pathname.match(/\/[0-9]+.*/)) {
         }
     }
 
+    /**
+     * 为独轮车按钮绑定开关
+     */
     WHEEL_BUTTON.onclick = () => {
         toggle()
         WHEEL_BUTTON.src = WHEEL_CONFIG.WHEEL_RUNNING ? WHEEL_CONFIG.WHEEL_START_IMG : WHEEL_CONFIG.WHEEL_PAUSE_IMG
     }
-
+    /**
+     * 设置样式并添加到document中
+     */
     WHEEL_BUTTON.style.cursor = 'pointer'
     WHEEL_BUTTON.style.display = 'inline-block'
     WHEEL_BUTTON.width = WHEEL_BUTTON.height = 100
@@ -89,11 +144,13 @@ if (location.pathname.match(/\/[0-9]+.*/)) {
     WHEEL_WRAPPER.height = 150
     WHEEL_WRAPPER.append(WHEEL_BUTTON)
     document.body.append(WHEEL_WRAPPER)
+    /**
+     * 调用background的getOption服务获取选项
+     */
     chrome.runtime.sendMessage({service:'getOption',params:{}}, res => {
         if (res.code == 0) {
             let options = res.data
             WHEEL_DATA.msg = options.wheelText
-            console.log(WHEEL_DATA)
             WHEEL_CONFIG.WHEEL_INTERVAL = options.wheelInterval
             WHEEL_CONFIG.WHEEL_START_IMG = options.wheelStartImg
             WHEEL_CONFIG.WHEEL_PAUSE_IMG = options.wheelPauseImg
