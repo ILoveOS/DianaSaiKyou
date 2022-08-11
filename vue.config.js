6e902dd5d819e71c5d0b4ad6d477d3149b02618c
ILoveOS <2929700634@qq.com>
2022-08-11 09:13:29 +0800

    添加了注释

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,31 @@
+/**
+ * vue生产配置
+ */
 const { defineConfig } = require('@vue/cli-service')
-const NodePolyfillPlugin=require('node-polyfill-webpack-plugin')
+/**
+ * 引入Polyfill插件让项目可以使用Nodejs的crypto和buffer模块
+ */
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
 module.exports = defineConfig({
+  /**
+   * 使用Polyfill插件
+   */
   configureWebpack: {
-    plugins:[
+    plugins: [
       new NodePolyfillPlugin()
     ]
   },
   transpileDependencies: true,
+  /**
+   * 页面清单
+   */
   pages: {
     popup: {
+      /**入口文件 */
       entry: './src/popup_main.js',
+      /**模板文件 */
       template: './public/popup.html',
+      /**生成的文件名 */
       filename: 'popup.html'
     },
     tab: {
